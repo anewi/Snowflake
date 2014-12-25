98ad9c8446bd1436f94cdf2cacdd14a1d7d3b2aa
Anna Winters <anna@winters.ws>
2014-12-26 00:30:09 +0100

    Now draws a correct Koch snowflake!

--- snowflake.cpp
+++ snowflake.cpp
@@ -24,12 +24,16 @@
 struct snowflake_vertex {
   double x;
   double y;
+  double centre_x; //Centre co-ordinates are used to ensure new equilateral triangles point outwards
+  double centre_y; 
 };
 
-inline snowflake_vertex create_snowflake_vertex(double x, double y) {
+inline snowflake_vertex create_snowflake_vertex(double x, double y, double centre_x, double centre_y) {
   snowflake_vertex vertex;
   vertex.x = x;
   vertex.y = y;
+  vertex.centre_x = centre_x;
+  vertex.centre_y = centre_y;
   return vertex;
 }
 
@@ -41,7 +45,7 @@ inline snowflake_vertex create_snowflake_vertex(double x, double y) {
 void generate_initial_triangle(std::vector<snowflake_vertex>* triangle, double x, double y, double length) {
   double side_length, height;
 
-  (*triangle).push_back(create_snowflake_vertex(x, y-length)); //Add top vertex, which is simply length distance above the centre
+  (*triangle).push_back(create_snowflake_vertex(x, y-length, x, y)); //Add top vertex, which is simply length distance above the centre
 
   //Calculate length of all of the sides of the triangle using cosine rule and known angles/lengths
   side_length = sqrt(pow(length, 2.0) + pow(length, 2.0) - 2*length*length*cos((120 * M_PI)/ 180));
@@ -49,9 +53,9 @@ void generate_initial_triangle(std::vector<snowflake_vertex>* triangle, double x
   //The side of the triangle, half of the base and the height form a right-angled triangle. Use pythag to calculate triangle height.
   height = sqrt(pow(side_length, 2.0)-pow(side_length/2.0, 2.0));
 
-  (*triangle).push_back(create_snowflake_vertex(x-(side_length/2.0), y-length+height)); //Bottom-left vertex
+  (*triangle).push_back(create_snowflake_vertex(x-(side_length/2.0), y-length+height, x, y)); //Bottom-left vertex
   
-  (*triangle).push_back(create_snowflake_vertex(x+(side_length/2.0), y-length+height)); //Bottom-right vertex
+  (*triangle).push_back(create_snowflake_vertex(x+(side_length/2.0), y-length+height, x, y)); //Bottom-right vertex
 }
 
 void draw_snowflake(SDL_Renderer* renderer, std::vector<snowflake_vertex>* snowflake) {
@@ -100,7 +104,6 @@ void draw_snowflake_antialiased(SDL_Renderer* renderer, std::vector<snowflake_ve
     shade = s*64;
     SDL_SetRenderDrawColor(renderer, shade, shade, shade, 255);
     SDL_RenderDrawPoints(renderer, screen[s].data(), screen[s].size());
-    std::cout << screen[s].size() << std::endl;
   }
 
 }
@@ -114,8 +117,9 @@ void iterate_snowflake(std::vector<snowflake_vertex>* previous_snowflake, std::v
   int end_vertex_offset; //Used to take into account that the final vertex connects up with the first
   double x1, y1, x2, y2, x3, y3; //Co-ordinates of the vertices of each new equilateral triangle
   double xd, yd; //The differences in x and y across the base of the new triangle
+  double xm, ym; //Co-ordinates of the midpoint of the base of the new triangle
   double side_length, height; //Side lengths and height of the new equilateral triangle
-  double xh, yh; //The differences in x and y across the height of the new triangle (perpendicular to xd, yd)
+  double xh, yh; //The differences in x and y across the height of the new triangle (perpendicular to xd, yd, passing through xm, ym)
 
   (*snowflake).clear(); //Empty the vector we will write into
 
@@ -124,31 +128,45 @@ void iterate_snowflake(std::vector<snowflake_vertex>* previous_snowflake, std::v
 
     end_vertex_offset = (i == (*previous_snowflake).size() - 1 ? 0 : i + 1); //0 if at end; i + 1 otherwise
 
+    //Calculate co-ordinates one third down the line
     x1 = ((*previous_snowflake)[i].x * 2.0 + (*previous_snowflake)[end_vertex_offset].x * 1.0) / 3.0;
     y1 = ((*previous_snowflake)[i].y * 2.0 + (*previous_snowflake)[end_vertex_offset].y * 1.0) / 3.0;
 
+    //Two thirds down the line
     x3 = ((*previous_snowflake)[i].x * 1.0 + (*previous_snowflake)[end_vertex_offset].x * 2.0) / 3.0;
     y3 = ((*previous_snowflake)[i].y * 1.0 + (*previous_snowflake)[end_vertex_offset].y * 2.0) / 3.0;
 
     xd = x1 - x3;
     yd = y1 - y3;
 
+    //Co-ordinates in the middle of the line
+    xm = (x1 + x3)/2.0;
+    ym = (y1 + y3)/2.0;
+
     side_length = sqrt(pow(xd, 2.0) + pow(yd, 2.0)); //Calculate side length using pythag on differences in x, y co-ords
 
     height = sqrt(pow(side_length, 2.0) - pow(side_length/2.0, 2.0)); //Calculate triangle height from right-angled triangle with side length as hypotenuse
 
-    yh = sqrt(pow(height, 2.0) / (pow(yd, 2.0) / pow(xd, 2.0) + 1));
+    yh = sqrt(pow(height, 2.0) / (pow(yd, 2.0) / pow(xd, 2.0) + 1)); //Calculate change in y between triangle base and triangle tip
 
-    //    yh = -(xd/yd)*sqrt(pow(height, 2.0) - pow(yd, 2.0));
+    xh = -(yd*yh)/xd; //From xdxh + ydyh = 0 (dot product of perpendicular lines being 0)
 
-    xh = -(yd*yh)/xd;
+    //Calculate co-ordinates of the tip of the new triangle: the middle of the base plus the height
+    x2 = xm + xh; 
+    y2 = ym + yh;
 
-    x2 = (x1 + x3)/2.0 + xh;
-    y2 = (y1 + y3)/2.0 + yh;
+    //Ensure new triangle points outwards by flipping the sign if it makes the tip further from the "centre" held by the snowflake_vertex for this purpose
+    if(hypot(x2 - (*previous_snowflake)[i].centre_x, y2 - (*previous_snowflake)[i].centre_y) < hypot((xm - xh) - (*previous_snowflake)[i].centre_x, (ym - yh) - (*previous_snowflake)[i].centre_y)){
+      x2 = xm - xh;
+      y2 = ym - yh;
+    }
 
-    (*snowflake).push_back(create_snowflake_vertex(x1, y1));
-    (*snowflake).push_back(create_snowflake_vertex(x2, y2));
-    (*snowflake).push_back(create_snowflake_vertex(x3, y3));
+    //Create first bottom right and tip of triangle vertices, using the centre of the base as the centre for the next iteration
+    (*snowflake).push_back(create_snowflake_vertex(x1, y1, xm, ym));
+    (*snowflake).push_back(create_snowflake_vertex(x2, y2, xm, ym));
+
+    //The third vertex uses the previous centre, because the line that follows it is not part of this equilateral triangle
+    (*snowflake).push_back(create_snowflake_vertex(x3, y3, (*previous_snowflake)[i].centre_x, (*previous_snowflake)[i].centre_y));
   }
 }
 
@@ -160,13 +178,10 @@ int main(int argc, char *argv[]) {
   std::vector<snowflake_vertex> render_snowflake;
 
   SDL_Init(SDL_INIT_VIDEO);
-  SDL_CreateWindowAndRenderer(800, 800, SDL_WINDOW_OPENGL, &window, &renderer);
+  SDL_CreateWindowAndRenderer(800, 800, 0, &window, &renderer);
   SDL_SetWindowTitle(window, "Snowflake");
 
-  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 16);
-
-  for (int i = 0; i < 12; i++) {
+  for (int i = 0; i < 12; i++) { //Iterations of snowflake
     if (i == 0) {
       generate_initial_triangle(&buffer_snowflake, 400.0, 400.0, 300.0);
     } else {
